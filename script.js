bbcdc70bdc319d05e13b2e8d031906fffcab2a86
Essietasha <163308907+Essietasha@users.noreply.github.com>
2024-07-11 03:21:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//Auto-Navigation
+// var counter = 1;
+// const labelEl = document.querySelectorAll('label');
+
+// setInterval(() => {
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+
+//     if(counter > labelEl.length){
+//         counter = 1;
+//     }
+// }, 2000);
+// console.log(labelEl);
